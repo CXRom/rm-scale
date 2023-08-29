08271066f174871dac3aff3a3401e3e883e8595c
Rommel Meza <rommel.meza@lifebeacon.com>
2023-08-29 09:37:31 -0600

    Add BLE

--- main/main.c
+++ main/main.c
@@ -6,6 +6,13 @@
 #include "esp_err.h"
 #include "esp_log.h"
 
+#include "nvs_flash.h"
+#include "esp_nimble_hci.h"
+#include "nimble/nimble_port.h"
+#include "nimble/nimble_port_freertos.h"
+#include "host/ble_hs.h"
+#include "services/gap/ble_svc_gap.h"
+
 #include "hx711_handler.h"
 #include "display_handler.h"
 
@@ -15,6 +22,25 @@ const char *TAG = "rm.scale";
 #define I2C_MAIN_SPEED (400 * 1000)
 #define I2C_MAIN_SDA 4
 #define I2C_MAIN_SCL 5
+
+void ble_store_util_sync_cb(void)
+{
+    ble_addr_t addr;
+    ble_hs_id_gen_rnd(1, &addr);
+    ble_hs_id_set_rnd(addr.val);
+
+    uint8_t uuid128[16];
+    memset(uuid128, 0x11, sizeof(uuid128));
+    ble_ibeacon_set_adv_data(uuid128, 2, 10, -50);
+
+    struct ble_gap_adv_params adv_params = (struct ble_gap_adv_params){0};
+    ble_gap_adv_start(BLE_OWN_ADDR_RANDOM, NULL, BLE_HS_FOREVER, &adv_params, NULL, NULL);
+}
+
+void host_task(void *param)
+{
+    nimble_port_run();
+}
 
 void app_main(void)
 {
@@ -29,6 +55,18 @@ void app_main(void)
     };
     ESP_ERROR_CHECK(i2c_param_config(I2C_MASTER, &i2c_conf));
     ESP_ERROR_CHECK(i2c_driver_install(I2C_MASTER, i2c_conf.mode, 0, 0, 0));
+
+    ESP_LOGI(TAG, "Initialize BLE");
+    nvs_flash_init();
+    int ret = nimble_port_init();
+    if (ret != ESP_OK)
+    {
+        ESP_LOGE(TAG, "nimble_port_init() failed with error: %d", ret);
+        return;
+    }
+
+    ble_hs_cfg.sync_cb = ble_store_util_sync_cb;
+    nimble_port_freertos_init(host_task);
 
     ESP_LOGI(TAG, "Initialize SSD1306");
     display_init(I2C_MASTER);
